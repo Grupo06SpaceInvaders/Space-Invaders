b2c03b0a658836b7d18293d18f906ce0fcd63c42
Lucas de Lima <lucaslsilva64@gmail.com>
2025-06-08 18:09:42 -0300

    Menu Principal Adição
    -Opções com musica

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,6 +5,7 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject menuprincipal;
     public GameObject menuhighscore;
+    public GameObject slideropcoes;
 
     public void Jogar()
     {
@@ -20,6 +21,18 @@ public class MainMenuController : MonoBehaviour
     {
         menuhighscore.SetActive(false);
         menuprincipal.SetActive(true);
+    }
+
+    public void Opcoes()
+    {
+        menuprincipal.SetActive(false);
+        slideropcoes.SetActive(true);
+    }
+
+    public void Voltarmenuopcoes()
+    {
+        menuprincipal.SetActive(true);
+        slideropcoes.SetActive(false);
     }
 
     public void Sair()
