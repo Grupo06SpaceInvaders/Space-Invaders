95e324a5dcacc730f4fe127622d0413129969a16
RafaeladaPuc <Rafaelalorenzo27@gmail.com>
2025-06-11 15:43:42 -0300

    Update TiroController.cs

--- Assets/Scripts/TiroController.cs
+++ Assets/Scripts/TiroController.cs
@@ -4,7 +4,6 @@ public class TiroController : MonoBehaviour
 {
     public float velocidade = 100f;
     public float tempoDestruir = 5f;
-
     void Start()
     {
         Destroy(this.gameObject, tempoDestruir);
@@ -12,7 +11,7 @@ public class TiroController : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+        transform.Translate(Vector3.up * velocidade * Time.deltaTime);
     }
 
     void OnCollisionEnter(Collision collision)
